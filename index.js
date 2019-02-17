ff8679dd3dc9e1460e6dcb0d2939f3ccd869d57e
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-02-17 03:23:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
     return "I can't hear you!"
     
-  } else if(string.toUpperCase() === string)
-  return "YES INDEED!" 
+  } else if(string.toUpperCase() === string){
+    return "YES INDEED!" 
+  } else{
+    return "I love you, too!"
+  }
   end
 }
